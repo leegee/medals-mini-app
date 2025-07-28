e24896b9ffb7169ef48480f28ee3bd2e6638fe23
Lee Goddard <leegee@gmail.com>
2025-07-28 11:07:58 +0200

    test coverage

--- src/lib/sort-medal-data.test.ts
+++ src/lib/sort-medal-data.test.ts
@@ -5,9 +5,28 @@ const winner: ICountryMedals = { gold: 3, silver: 2, bronze: 1, code: 'SWE' };
 const second: ICountryMedals = { gold: 3, silver: 1, bronze: 1, code: 'NOR' };
 const third: ICountryMedals = { gold: 2, silver: 4, bronze: 2, code: 'RUS' };
 
-
 describe('sortMedals', () => {
-    test('sort by gold', () => {
+    test('sort by total', () => {
+        expect(compareMedals(winner, winner, 'total')).toBe(0);
+        expect(compareMedals(winner, second, 'total')).toBeLessThan(0);
         expect(compareMedals(winner, third, 'total')).toBeGreaterThan(0);
+    });
+
+    test('sort by gold', () => {
+        expect(compareMedals(winner, winner, 'gold')).toBe(0);
+        expect(compareMedals(winner, second, 'gold')).toBeLessThan(0);
+        expect(compareMedals(winner, third, 'gold')).toBeLessThan(0);
+    });
+
+    test('sort by silver', () => {
+        expect(compareMedals(winner, winner, 'silver')).toBe(0);
+        expect(compareMedals(winner, second, 'silver')).toBeLessThan(0);
+        expect(compareMedals(winner, third, 'silver')).toBeGreaterThan(0);
+    });
+
+    test('sort by bronze', () => {
+        expect(compareMedals(winner, winner, 'bronze')).toBe(0);
+        expect(compareMedals(winner, second, 'bronze')).toBe(0);
+        expect(compareMedals(winner, third, 'bronze')).toBeGreaterThan(0);
     });
 });
