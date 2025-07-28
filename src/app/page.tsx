8d23bb818fecaa377703df8a150ea8706b30c169
Lee Goddard <leegee@gmail.com>
2025-07-28 09:24:07 +0200

    Notes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { ICountryMedals, ISortKey } from '@/types/types';
 import { getMedalData } from '@/lib/get-medal-data';
 import MedalTable from '@/components/MedalTable';
 
-// Brief says load the data only once:
+// Brief says load the data only once: this forces caching of all fetches
 export const dynamic = 'force-static';
 
 type IMedalPageProps = {
