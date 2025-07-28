f63c3f5049ab28d99318ab6671ed7770b69b7d3a
Lee Goddard <leegee@gmail.com>
2025-07-28 09:11:27 +0200

    Sort to spec - untested

--- src/lib/sort-medal-data.ts
+++ src/lib/sort-medal-data.ts
@@ -1,18 +1,34 @@
 import { ICountryMedals, ISortKey } from "@/types/types";
 
+/**
+ * 
+ * Sort Ranking 
+ * a. When ranking by total medals, ties are broken by most gold.  
+ * b. When ranking by gold, ties are broken by most silver.  
+ * c. When ranking by silver, ties are broken by most gold.  
+ * d. When ranking by bronze, ties are broken by most gold.
+ * 
+ * In other words, the tie-breaker is always the gold count, 
+ * unless sorting by gold, in which case it is the silver count.
+ *
+ * @param a 
+ * @param b 
+ * @param sortBy 
+ * @returns 
+ */
 export function sortMedals(a: ICountryMedals, b: ICountryMedals, sortKey: ISortKey) {
     const aTotal = a.gold + a.silver + a.bronze;
     const bTotal = b.gold + b.silver + b.bronze;
 
     switch (sortKey) {
         case 'total':
-            return bTotal - aTotal;
+            return (bTotal - aTotal) || (b.gold - a.gold);
         case 'gold':
-            return b.gold - a.gold;
+            return (b.gold - a.gold) || (b.silver - a.silver);
         case 'silver':
-            return b.silver - a.silver;
+            return (b.silver - a.silver) || (b.gold - a.gold);;
         case 'bronze':
-            return b.bronze - a.bronze;
+            return (b.bronze - a.bronze) || (b.gold - a.gold);
         default:
             return 0;
     }
