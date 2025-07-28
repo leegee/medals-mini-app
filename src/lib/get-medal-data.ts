2dbc7c261577414a4bd95e70a9047dfe0bdea5b1
Lee Goddard <leegee@gmail.com>
2025-07-28 11:12:59 +0200

    Notes on incompleteness

--- src/lib/get-medal-data.ts
+++ src/lib/get-medal-data.ts
@@ -1,8 +1,20 @@
 import medals from '../../public/medals.json';
 
 export function getMedalData() {
-    // API call for medal data would go here
-    console.log('Getting data');
-    // TODO use a fetch request as per brief
     return Promise.resolve(medals);
 }
+
+/*
+
+TODO: Set an env var for both dev and prod to point to the medal.json / final API endpoint: I ran out of time.
+
+export async function getMedalData() {
+    // API call for medal data would go here
+    console.log('Getting data');
+    const res = await fetch('/medals.json');
+    if (!res.ok) {
+        throw new Error(`Failed to fetch medals: ${res.statusText}`);
+    }
+    return res.json();
+}
+*/
