5b5a7e797d44256664c92788f63bd1f297616f7e
Lee Goddard <leegee@gmail.com>
2025-07-28 09:31:43 +0200

    Note

--- src/lib/get-medal-data.ts
+++ src/lib/get-medal-data.ts
@@ -3,5 +3,6 @@ import medals from '../../public/medals.json';
 export function getMedalData() {
     // API call for medal data would go here
     console.log('Getting data');
+    // TODO use a fetch request as per brief
     return Promise.resolve(medals);
 }
